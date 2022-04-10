b9343b92b59ae8242bab89ef9592703bf1a1dced
Nadine Haninta <nadinehaninta@gmail.com>
2022-04-10 14:50:33 +0700

    smh mh

--- apiemoney.sql
+++ apiemoney.sql
@@ -32,7 +32,7 @@ CREATE TABLE `user` (
   `email` varchar(20) NOT NULL,
   `notelp` int NOT NULL,
   `pass` varchar(30) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `user`
