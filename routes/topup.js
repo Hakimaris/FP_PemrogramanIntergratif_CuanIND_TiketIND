890d4ab9f5c09ce0288b95d084789f541e444b28
Nadine Haninta <nadinehaninta@gmail.com>
2022-04-11 20:31:17 +0700

    hehe

--- routes/topup.js
+++ routes/topup.js
@@ -6,12 +6,11 @@ const db = require('../db');
 const router = Router();
 
 router.post('/', async(req, res) => {
-    // res.send("sudah topup")
     const { duid, token, id} = req.body;
     jwt.verify(token, 'buatdebug', (err, response) => {
         console.log(response);
     })
-    const query = await db.promise().query(`update user_money from user set user_money=user_money+${duid} where user_id=1`);
+    const query = await db.promise().query(`UPDATE user_money FROM user SET user_money=user_money+${duid} WHERE user_id=1`);
 });
 
 module.exports = router;
