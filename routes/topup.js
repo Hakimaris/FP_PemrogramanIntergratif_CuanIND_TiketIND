abea21725053b333f5d6757919e2643705931ffb
Nadine Haninta <nadinehaninta@gmail.com>
2022-04-11 21:00:37 +0700

    a

--- routes/topup.js
+++ routes/topup.js
@@ -8,8 +8,12 @@ const router = Router();
 router.post('/', async(req, res) => {
     const { duid, token, id} = req.body;
     jwt.verify(token, 'buatdebug', (err, response) => {
-        console.log(response);
+        var userID = decoded.id;
+        console.log(response + userID);
     })
+    // const decoded = jwt.verify(token, 'buatdebug');  
+    // var userId = decoded.id  
+    // res.status(200).json(userId);
     const query = await db.promise().query(`UPDATE user SET user_money = user_money+ ${duid} WHERE user.user_id = 1;`);
 });
 
