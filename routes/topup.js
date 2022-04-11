608229252db2a332274b25882d44d933d1063ada
Hakim <Hakimfatih30@gmail.com>
2022-04-11 21:12:51 +0700

    kelar topup

--- routes/topup.js
+++ routes/topup.js
@@ -5,11 +5,17 @@ const router = Router();
 
 router.post('/', async(req, res) => {
     // res.send("sudah topup")
-    const { duid, token, id} = req.body;
+    const { duid, token, id } = req.body;
+    let userid;
     jwt.verify(token, 'buatdebug', (err, response) => {
-        console.log(response);
+        // console.log(response);
+        if (err) {
+            return res.status(408).send("salah token");
+        }
+        userid = response;
     })
-    const query = await db.promise().query(`UPDATE user SET user_money = user_money+ ${duid} WHERE user.user_id = 1;`);
+    const query = await db.promise().query(`UPDATE user SET user_money = user_money+ ${duid} WHERE user_id = ${userid.id};`);
+    res.status(200).send("udah terupdate")
 });
 
 module.exports = router;
