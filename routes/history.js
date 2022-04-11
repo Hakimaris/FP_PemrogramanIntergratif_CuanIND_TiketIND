da435fa64db330de6d7ac839041e434778fd9656
Nadine Haninta <nadinehaninta@gmail.com>
2022-04-11 15:31:40 +0700

    Update DB

--- routes/history.js
+++ routes/history.js
@@ -4,7 +4,7 @@ const db = require('../db');
 const router = Router();
 
 router.get('/', async (req, res) => {
-    const queryresult = await db.promise().query('select * from namatabelbuathistory');
+    const queryresult = await db.promise().query('select * from item');
     res.status(200).send(queryresult[0]);
 })
 
