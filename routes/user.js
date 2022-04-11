64cb09139a32a5ba3e969b75a9bf41968b0b39c2
Hakim <Hakimfatih30@gmail.com>
2022-04-11 16:52:38 +0700

    delet history.js, isiny dipindah ke user

--- routes/user.js
+++ routes/user.js
@@ -3,9 +3,10 @@ const jwt = require('jsonwebtoken');
 const db = require('../db');
 const router = Router();
 
-router.get('/history', (req, res) => {
-    res.send("sudah dpt history")
-});
+router.get('/history', async (req, res) => {
+    const queryresult = await db.promise().query('select * from item');
+    res.status(200).send(queryresult[0]);
+})
 
 router.post('/login', (req, res) => {
     res.send("sudah dpt login")
