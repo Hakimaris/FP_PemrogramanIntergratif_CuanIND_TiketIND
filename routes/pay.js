c24820a965a4061ba6860adce72bc968002cd6f4
Hakim <Hakimfatih30@gmail.com>
2022-04-19 12:43:09 +0700

    conditioning pay.js

--- routes/pay.js
+++ routes/pay.js
@@ -3,11 +3,16 @@ require('dotenv').config();
 const { Router } = require('express');
 const jwt = require('jsonwebtoken');
 const db = require('../db');
+const { authenticateToken } = require('../midelwer');
 const router = Router();
 
-router.post('/',(req, res) => {
-    const {barang,kuantitas,token} = req.body
-    res.send("sudah topup")
+router.post('/',authenticateToken,async(req, res) => {
+    const {notelp,idbarang} = req.body
+    let resuld = req.response;
+    // logic sql start here tq
+
+    // logic sql ends here
+    res.status(200).json("tiket dengan id barang " + idbarang); // BELOM KELAR
 });
 
 module.exports = router;
