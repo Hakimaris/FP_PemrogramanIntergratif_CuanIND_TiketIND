5e94d538c196f48d76ababda0d43aa0d5ce3eaf7
Nadine Haninta <nadinehaninta@gmail.com>
2022-04-19 12:42:55 +0700

    Update Transfer and Top Up Mech

--- routes/transfer.js
+++ routes/transfer.js
@@ -10,15 +10,13 @@ router.post('/', authenticateToken,async(req, res) => {
     const { amount, target } = req.body;
     let resuld = req.response;
 
-    //BELUM SELESAI
-    const query0 = await db.promise().query(`UPDATE user SET user_money = user_money- ${amount} WHERE user_number = ${userid.id};`);
+    const query0 = await db.promise().query(`UPDATE user SET user_money = user_money- ${amount} WHERE user_number = ${resuld.notelp};`);
     const query1 = await db.promise().query(`UPDATE user SET user_money = user_money+ ${amount} WHERE user_number = ${target};`);
-    const query2 = await db.promise().query(`SELECT user_id FROM user WHERE user_number = ${target};`);
-    let hasilQuery1 = query2[0][0];
-    const query3 = await db.promise().query(`INSERT INTO item (item_id, item_name, item_value, item_stock, item_actor) VALUES (NULL, 'Transfer', ${amount}, '1', ${hasilQuery1.user_id});`);
-    const query4 = await db.promise().query(`SELECT item_id FROM item WHERE item_actor=${hasilQuery1.user_id} AND item_value=${amount} ORDER BY item_id DESC LIMIT 1;`);
-    let hasilQuery2 = query4[0][0];
-    const query5 = await db.promise().query(`INSERT INTO history (history_id, history_user, history_item, history_quantity) VALUES (NULL, ${userid.id}, ${hasilQuery2.item_id}, 1);`);
+    const query2 = await db.promise().query(`INSERT INTO receipt (receipt_id, receipt_send, receipt_receive, receipt_value) VALUES (NULL, ${resuld.notelp}, ${target}, ${amount});`);
+    const query3 = await db.promise().query(`SELECT receipt_id FROM receipt WHERE receipt_send=${resuld.notelp} AND receipt_receive=${target} AND receipt_value=${amount} ORDER BY receipt_id DESC LIMIT 1;`);
+    let hasilQuery3 = query3[0][0];
+    const query4 = await db.promise().query(`INSERT INTO history (history_id, history_user, history_cat, history_receipt) VALUES (NULL, ${resuld.notelp}, 2, ${hasilQuery3.receipt_id});`);
+    const query5 = await db.promise().query(`INSERT INTO history (history_id, history_user, history_cat, history_receipt) VALUES (NULL, ${target}, 1, ${hasilQuery3.receipt_id});`);
     res.status(200).json("Transfer berhasil")
 });
 
